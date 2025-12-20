3f3501c4ff8668bd80687f1f9dfb685eaa644fcd
Linkdash2019 <csmcclellan@outlook.com>
2025-12-19 23:42:15 -0700

    Randomize starting position. Patch castle added to pool. Random seeds. Removed obsolete castle portal. Removed broken door relocking

--- main.py
+++ main.py
@@ -4,19 +4,20 @@ import time
 import dolphin_memory_engine as dme
 import re
 
-items = ['Fountain Gardens', 'Flower Fields', 'Rainbow Falls', 'Big-Bean Vine ', 'Mole Hole', 'Weird Woods', 'Pyramid Sands', 'Lava Landing', 'Cool Cave', 'Dino Jungle', 'Temper Temple', 'Dusk Dunes', 'Toy Tracks', 'Mushroom Run', 'Sweets Park', 'Melody Town', 'Cocoa Station', 'Dark Manor', 'Splash Beach', 'Blub-Blub Ocean', 'Secret Island', 'Deep-Dive Deep', 'Boom Boatyard', 'Fossil Reef', 'Snowy Fields', 'Cozy Cabin', 'Mt. Slide', 'Frosty Wheel', 'Frigid Fjords', 'Evergreen Lift', 'Future City', 'Tube Town', 'Mysterious UFO', 'Stellar Way', 'Moon Base', 'Outer Rings', "Whispy's Forest", 'Tempest Towers', 'Cloud Palace', 'Castle Dedede', 'Meta Melon Isle', 'Battleship Halberd', 'Fangora', 'Hot Wings', 'Squashini', 'Capamari', 'King Dedede', 'Meta Knight', 'Yin-Yarn']
+items = ['Patch Castle', 'Fountain Gardens', 'Flower Fields', 'Rainbow Falls', 'Big-Bean Vine ', 'Mole Hole', 'Weird Woods', 'Pyramid Sands', 'Lava Landing', 'Cool Cave', 'Dino Jungle', 'Temper Temple', 'Dusk Dunes', 'Toy Tracks', 'Mushroom Run', 'Sweets Park', 'Melody Town', 'Cocoa Station', 'Dark Manor', 'Splash Beach', 'Blub-Blub Ocean', 'Secret Island', 'Deep-Dive Deep', 'Boom Boatyard', 'Fossil Reef', 'Snowy Fields', 'Cozy Cabin', 'Mt. Slide', 'Frosty Wheel', 'Frigid Fjords', 'Evergreen Lift', 'Future City', 'Tube Town', 'Mysterious UFO', 'Stellar Way', 'Moon Base', 'Outer Rings', "Whispy's Forest", 'Tempest Towers', 'Cloud Palace', 'Castle Dedede', 'Meta Melon Isle', 'Battleship Halberd', 'Fangora', 'Hot Wings', 'Squashini', 'Capamari', 'King Dedede', 'Meta Knight', 'Yin-Yarn']
 locations = ['Patch Castle Goal', 'Fountain Gardens Goal', 'Flower Fields Goal', 'Rainbow Falls Goal', 'Big-Bean Vine Goal', 'Mole Hole Goal', 'Weird Woods Goal', 'Pyramid Sands Goal', 'Lava Landing Goal', 'Cool Cave Goal', 'Dino Jungle Goal', 'Temper Temple Goal', 'Dusk Dunes Goal', 'Toy Tracks Goal', 'Mushroom Run Goal', 'Sweets Park Goal', 'Melody Town Goal', 'Cocoa Station Goal', 'Dark Manor Goal', 'Splash Beach Goal', 'Blub-Blub Ocean Goal', 'Secret Island Goal', 'Deep-Dive Deep Goal', 'Boom Boatyard Goal', 'Fossil Reef Goal', 'Snowy Fields Goal', 'Cozy Cabin Goal', 'Mt. Slide Goal', 'Frosty Wheel Goal', 'Frigid Fjords Goal', 'Evergreen Lift Goal', 'Future City Goal', 'Tube Town Goal', 'Mysterious UFO Goal', 'Stellar Way Goal', 'Moon Base Goal', 'Outer Rings Goal', "Whispy's Forest", 'Tempest Towers Goal', 'Cloud Palace Goal', 'Castle Dedede Goal', 'Meta Melon Isle Goal', 'Battleship Halberd Goal', 'Fangora Goal', 'Hot Wings Goal', 'Squashini Goal', 'Capamari Goal', 'King Dedede Goal', 'Meta Knight Goal']
 
 unrandom_items = items[:]
 unrandom_locations = locations[:]
-
-seed = input('What is the seed? >>> ')
-#seed = 876
-seed = "".join(re.findall(r'\d', seed))
+try:
+    seed = int(input('What is the seed?\nLeave blank for a random seed\nNumbers ONLY\n>>> '))
+except ValueError:
+    seed = int(time.time())
+    print("Seed is:", seed)
 random.seed(seed)
 random.shuffle(items)
+firstItem = items.pop(0)
 item_placement = dict(zip(locations, items))
-print(item_placement)
 
 def setup():
     print("Running Setup")
@@ -30,7 +31,7 @@ def setup():
 
     #Watch Cutscenes
     #Refer to GitHub for more info
-    #TODO Finish  finding all cutscenes
+    #TODO Finish finding all cutscenes
     dme.write_byte(0x906A962B, 0x03)
     dme.write_byte(0x906A96F7, 0x03)
     dme.write_byte(0x906A973F, 0x03)
@@ -40,12 +41,48 @@ def setup():
     #Unlock Map
     dme.write_byte(0x906A7007, 0x10)
 
-    #Unlock Patch Castle
-    dme.write_byte(0x906A7067, 0x02)
+    #Unlock First Level
+    hops = 0
+    for item in unrandom_items:
+        if item == firstItem:
+            dme.write_byte(0x906A7067+(hops*36), 0x02)
+            print("START unlocked", item, "\nLevel id is:", hops)
+            break
+        hops += 1
 
     #Set Kirby Location
-    #Can be any value between 0-7
-    change_saved_location(0)
+    #Normal Levels
+    if hops <= 0:
+        change_saved_location(0)
+    elif hops <= 6:
+        change_saved_location(1)
+    elif hops <= 12:
+        change_saved_location(2)
+    elif hops <= 18:
+        change_saved_location(3)
+    elif hops <= 24:
+        change_saved_location(4)
+    elif hops <= 30:
+        change_saved_location(5)
+    elif hops <= 36:
+        change_saved_location(6)
+    elif hops <= 42:
+        change_saved_location(7)
+    #Boss Levels
+    elif hops <= 43:
+        change_saved_location(1)
+    elif hops <= 44:
+        change_saved_location(2)
+    elif hops <= 45:
+        change_saved_location(3)
+    elif hops <= 46:
+        change_saved_location(4)
+    elif hops <= 47:
+        change_saved_location(5)
+    elif hops <= 48:
+        change_saved_location(6)
+    elif hops <= 49:
+        change_saved_location(7)
     
 def check_medals(hops=0):
     offset = 8
@@ -62,7 +99,7 @@ def check_doors():
     while hops <= 49:
         if dme.read_byte(0x906A7067+(hops*level_offset)+offset) == 3:
             unlockNextLevel(hops)
-            relockTrouble(hops+1)
+            #relockTrouble(hops+1)
         hops += 1
 
 def change_saved_location(world=0):
@@ -71,23 +108,11 @@ def change_saved_location(world=0):
     string = f'A00{world}ROOMNONE'
     dme.write_bytes(0x906A700C, string.encode('ascii'))
 
-def castle_portal(levelNum):
-    #Change Patch castle level header to levelNum
-    level_offset = 36
-    offset = 1
-
-    if dme.read_byte(0x906A7067 + (levelNum * level_offset)) != 0x01:
-        levelHeader = dme.read_bytes(0x906A7067 + (levelNum * level_offset) + offset, 4)
-        dme.write_bytes(0x906A7067 + offset, levelHeader)
-    else:
-        print(unrandom_items[levelNum],"is not unlocked")
-
 def unlockNextLevel(hops):
     level_offset = 36
     unlock_hops = 0
 
     for item in unrandom_items:
-        unlock_hops += 1
         if items[hops] == item:
             if dme.read_byte(0x906A7067+(unlock_hops*level_offset)) == 0x01:
                 if item == 'Yin-Yarn':
@@ -97,25 +122,11 @@ def unlockNextLevel(hops):
                     dme.write_byte(0x906A7067+(unlock_hops*level_offset), 0x02)
                 print(unrandom_locations[hops], "unlocked",items[hops],"\nLevel id is:", unlock_hops)
             return
+        unlock_hops += 1
 
-#TODO
-def relockTrouble(levelNum):
-    offset = 0
-    level_offset = 36
-
-    if dme.read_byte(0x906A7067+(levelNum*level_offset)+offset) == 2:
-        unlock_hops = 0
-        lock_hops = 0
-
-        for item in items:
-            if item == unrandom_items[levelNum]:
-                for location in unrandom_items:
-                    if location == unrandom_items[unlock_hops]:
-                        print(unrandom_items[levelNum],"should get locked", )
-                        dme.write_byte(0x906A7067 + (levelNum * level_offset), 0x01)
-                        return
-                    lock_hops += 1
-                unlock_hops += 1
+#TODO lock levels that shouldn't be unlocked.
+def relockTrouble():
+    pass
 
 def hint(levelNum):
     unlock_hops = 0
@@ -148,17 +159,6 @@ def userConsoleLoop():
         if command == 'exit':
             return
 
-        elif command == 'portal':
-            try:
-                portalNum = int(input('Enter portal number (0-48): '))
-                if portalNum > 48:
-                    print("Invalid hint. Resetting to Patch Castle.")
-                    portalNum = 0
-            except ValueError:
-                print('Invalid portal number. Resetting to Patch Castle')
-                portalNum = 0
-            castle_portal(portalNum)
-
         elif command == 'warp':
             location = int(input("WARNING: Failure to follow instructions may result in a corrupt save (or worse corrupt your Wii system memory)\nPlease exit to the title screen then\nenter world number (0-6) >>> "))
             change_saved_location(location)
@@ -179,7 +179,6 @@ def userConsoleLoop():
             print(
                 "Unknown command\n"
                 "Valid commands are:\n"
-                "  -portal (Puts a portal at Patch Castles door)\n"
                 "  -warp (Teleport Kirby across space)\n"
                 "  -hint (Get a spoiler/hint)\n"
                 "  -exit (Exits the randomizer cleanly)"
@@ -193,7 +192,7 @@ def userConsoleLoop():
 dme.hook()
 print("Connected!")
 
-#Check if file 1 is empty
+#Check if file 1 is probably empty
 if dme.read_byte(0x906A962B) == dme.read_byte(0x906A96F7) == 0x01:
     setup()
 
