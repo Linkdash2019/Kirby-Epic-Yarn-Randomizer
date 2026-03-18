5b2bee4a1f5e2ae5c540f22b40047fe73fca8333
Linkdash2019 <csmcclellan@outlook.com>
2026-03-18 15:18:46 -0700

    Add proper logic

--- shuffle.py
+++ shuffle.py
@@ -9,8 +9,8 @@ def shuffle(setSeed=str(int(time.time()))):
     # Use settings
     var.seed = setSeed
     random.seed(var.seed)
-
     
+    var.items += var.nothingItem
     var.locations = var.locations | var.startLocation
     if var.shuffleDoors:
         var.items += var.doorItems
@@ -18,12 +18,54 @@ def shuffle(setSeed=str(int(time.time()))):
     if var.shuffleChests:
         var.items += var.chestItems
         var.locations = var.locations | var.chestLocations
-    var.items += var.nothingItem
 
-    var.unrandom_items = var.items[:]
+    var.spoiler = dict.fromkeys(var.locations, None)
 
     # Shuffle
-    random.shuffle(var.items)
+    if 'Yin-Yarn' in var.items:
+        items_to_place = var.items[:]
+        random.shuffle(items_to_place)
 
-    #Finish
-    var.spoiler = dict(zip(var.locations, var.items))
+        placed_items = set()  # Items already placed
+
+        while items_to_place:
+            # Find all accessible locations
+            accessible_locations = []
+            for location, requirements in var.locations.items():
+                if var.spoiler[location] is None:  
+                    if all(req in placed_items for req in requirements):
+                        accessible_locations.append(location)
+            print(f"Accessible locations: {accessible_locations}")
+
+       
+            if not accessible_locations and 'Yin-Yarn' not in placed_items:
+                #Undo last item placment
+                var.spoiler[lastSuccess[0]] = None
+                print(f"Undoing placement of {lastSuccess[1]} at {lastSuccess[0]} to ensure 'Yin-Yarn' can be placed")
+                items_to_place.append(lastSuccess[1])
+                placed_items.remove(lastSuccess[1])
+                continue
+
+
+            if not accessible_locations:
+                #Place current item in random slot
+                accessible_locations = [loc for loc, item in var.spoiler.items() if item is None]
+                print("Forced everywhere to be accessible")
+
+            if not accessible_locations:
+                break
+
+            # Place a random item at a random accessible location
+            location = random.choice(accessible_locations)
+            item = items_to_place.pop(0)
+            var.spoiler[location] = item
+            placed_items.add(item)
+            lastSuccess = (location, item)
+
+    else:
+        random.shuffle(var.items)
+        for item in var.items:
+            for location in var.locations:
+                if var.spoiler[location] is None:
+                    var.spoiler[location] = item
+                    break # Return to item loop
